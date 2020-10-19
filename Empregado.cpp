3cebe293f2e801e656417b6ccc8f38d9d4c80a22
Thiago Santos Oliveira <thiagosantos.on@gmail.com>
2020-10-19 19:54:15 -0300

    Update Empregado.cpp

--- Empregado.cpp
+++ Empregado.cpp
@@ -1,5 +1,6 @@
 #include "Empregado.hpp"
 
+#define MAXIMO_HORAS_TRABALHADAS 8
 #define ACRESCIMO_HORA_EXTRA 0,5
 
 void Empregado::setNome(std::string nome) { this->nome = nome; }
@@ -14,8 +15,8 @@ double Empregado::getQuotaMensalVendas() { return this->quotaMensalVendas;}
 double Empregado::pagamentoMensal(double horasTrabalhadas) {
     
     //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-    if(horasTrabalhadas > 8) {
-        double horasExtras = horasTrabalhadas - 8;
+    if(horasTrabalhadas > MAXIMO_HORAS_TRABALHADAS) {
+        double horasExtras = horasTrabalhadas - MAXIMO_HORAS_TRABALHADAS;
         horasTrabalhadas += horasExtras * ACRESCIMO_HORA_EXTRA;
     }
     return horasTrabalhadas * this->salarioHora;
